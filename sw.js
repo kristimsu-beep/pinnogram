0a8e57f37e4b55d02cd876284bcff6dc9ed20d05
kristimsu-beep <kristimsu@gmail.com>
2026-03-12 19:09:01 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,24 @@ self.addEventListener('push', function(event) {
     event.waitUntil(
         self.registration.showNotification(data.title, {
             body: data.body,
-            icon: 'https://i.ibb.co/4pSbxsh/user-avatar.png'
+            icon: 'https://i.ibb.co/4pSbxsh/user-avatar.png',
+            badge: 'https://i.ibb.co/4pSbxsh/user-avatar.png'
+        })
+    );
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({type: 'window', includeUncontrolled: true}).then(function(clientList) {
+            if (clientList.length > 0) {
+                let client = clientList[0];
+                for (let i = 0; i < clientList.length; i++) {
+                    if (clientList[i].focused) client = clientList[i];
+                }
+                return client.focus();
+            }
+            return clients.openWindow('/');
         })
     );
 });
